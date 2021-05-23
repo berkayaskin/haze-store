453d42f22aec3b590cac05ec958756268813acdd
berkayaskin <berkay.askin@bitechnology.com>
2021-05-23 19:23:13 +0300

    create shop page, collection preview and collection item components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
+import ShopPage from "./pages/homepage/shop/shop.component";
 
 function App() {
 	return (
 		<div>
 			<Switch>
 				<Route exact path="/" component={HomePage} />
+				<Route path="/shop" component={ShopPage} />
 			</Switch>
 		</div>
 	);
