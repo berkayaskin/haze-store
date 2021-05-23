01d08107de7bd3bed9dfa1efc71ff08728fae400
berkayaskin <berkay.askin@bitechnology.com>
2021-05-23 21:32:45 +0300

    create sign in and sign up page, sign in and custom button component

---
+++ src/components/custom-button/custom-button.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./custom-button.styles.scss";
+
+const CustomButton = ({ children, ...otherProps }) => {
+	return (
+		<div className="custom-button" {...otherProps}>
+			{children}
+		</div>
+	);
+};
+
+export default CustomButton;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import SHOP_DATA from "./shop.data";
-import CollectionPreview from "../../../components/collection-preview/collection-preview.component";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
 class ShopPage extends Component {
 	state = {
